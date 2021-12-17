1066690be0ca47b7a3bae792fac5515e323a1b26
Tam Le <tamsgit@gmail.com>
2021-12-16 22:13:08 -0500

    Zsh: Install git alias plugin from Oh My Zsh

--- setup-scripts/zsh.sh
+++ setup-scripts/zsh.sh
@@ -61,3 +61,23 @@ install_powerlevel10k_theme() {
 }
 [ ! -d ~/"$plugins_dir"/powerlevel10k ] && install_powerlevel10k_theme
 
+
+# Install git.plugin.zsh from Oh My Zsh framework (https://github.com/ohmyzsh/ohmyzsh)
+install_git_plugin_zsh() {
+
+	## Activate auto-completion system
+	if ! grep -q 'autoload -Uz compinit' ~/.zshrc; then
+		echo -e '\n# Activate auto-completion system' >> ~/.zshrc
+		echo 'autoload -Uz compinit' >> ~/.zshrc
+		echo 'compinit' >> ~/.zshrc
+	fi
+
+	cd ~/"$plugins_dir"
+	curl -O https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/git/git.plugin.zsh
+	if ! grep -q 'source ~/'"$plugins_dir"'/git.plugin.zsh' ~/.zshrc; then
+		echo -e '\n# Load git plugin from Oh My Zsh framework (https://github.com/ohmyzsh/ohmyzsh)' >> ~/.zshrc
+		echo '[ -f ~/'"$plugins_dir"'/git.plugin.zsh ] && source ~/'"$plugins_dir"'/git.plugin.zsh' >> ~/.zshrc
+	fi
+	cd - >/dev/null
+}
+install_git_plugin_zsh
