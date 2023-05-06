4afcc94fe6f1db2b8f2aedfbc813a06567fd702b
Tam Le <tamsgit@gmail.com>
2023-05-06 17:56:30 -0600

    Nvim-LSP: Activate auto-format upon file save

--- nvim/lua/lsp/formatter.lua
+++ nvim/lua/lsp/formatter.lua
@@ -17,7 +17,27 @@ null_ls.setup({
 		null_ls.builtins.formatting.djlint,
 		null_ls.builtins.diagnostics.ruff,
 	},
+
+	-- Trigger to format automatically upon save
+	-- Reference: https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Formatting-on-save
+	-- you can reuse a shared lspconfig on_attach callback here
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+					-- vim.lsp.buf.formatting_sync()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
 })
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 -- Filter LSP formatters so that only null-ls receives the formatting request
 -- Reference: https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Formatting-on-save#choosing-a-client-for-formatting
