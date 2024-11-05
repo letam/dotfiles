8efccb585dfcbd11449e4a7789eb95f64e152728
Tam Le <tamsgit@gmail.com>
2024-11-05 17:45:44 -0500

    Nvim: Update name of lspconfig ts_ls

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -6,7 +6,7 @@
 local lspconfig = require('lspconfig')
 -- lspconfig.pyright.setup {}
 lspconfig.pylsp.setup {}
-lspconfig.tsserver.setup {}
+lspconfig.ts_ls.setup {}
 lspconfig.rust_analyzer.setup {
   -- Server-specific settings. See `:help lspconfig-setup`
   settings = {
