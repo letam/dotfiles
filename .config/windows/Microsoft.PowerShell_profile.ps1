802ebfcc954fdb9db4f5d14f4ba0c4a8d1667fe4
Tam Le <tamsgit@gmail.com>
2026-01-04 14:35:42 -0500

    windows Add PowerShell profile config file

---
+++ .config/windows/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,17 @@
+# Save to $PROFILE
+# (you can open it via: `notepad $PROFILE`)
+
+Set-Alias -Name np -Value C:\Windows\notepad.exe
+
+Import-Module "$HOME\code\dotfiles\.config\windows\GitAliases.psm1" -DisableNameChecking
+
+# to allow duplicating tabs in current directory
+function prompt {
+   $loc = $executionContext.SessionState.Path.CurrentLocation;
+   $out = ""
+   if ($loc.Provider.Name -eq "FileSystem") {
+       $out += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+   }
+   $out += "PS $loc$('>' * ($nestedPromptLevel + 1)) "
+   return $out
+}
