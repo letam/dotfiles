958703272d9d9412ccb0d4749a3ebb7064d81d8b
Tam Le <tamsgit@gmail.com>
2026-01-06 16:08:24 -0500

    nvim-obsidian fix: Let user know if no vault found

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -1,4 +1,30 @@
 vim.opt.conceallevel = 2
+
+local dir_exists = function(p) return ((vim.uv or vim.loop).fs_stat(vim.fn.expand(p)) or {}).type == "directory" end
+
+default_workspaces = {
+    {
+      name = "personal",
+      path = "~/vaults/personal",
+    },
+    {
+      name = "work",
+      path = "~/vaults/work",
+    },
+}
+present_workspaces = {}
+is_workspace_present = false
+for _, x in pairs(default_workspaces) do
+  if dir_exists(x.path) then
+    is_workspace_present = true
+    table.insert(present_workspaces, x)
+  end
+end
+if not is_workspace_present then
+  print("obsidian.lua: No Obsidian workspace found.\nYou can create (or link to one) at '~/vaults/personal' or '~/vaults/workspace'.")
+  return {}
+end
+
 return {
   "obsidian-nvim/obsidian.nvim",
   version = "*", -- use latest release, remove to use latest commit
@@ -7,15 +33,6 @@ return {
   ---@type obsidian.config
   opts = {
     legacy_commands = false, -- this will be removed in the next major release
-    workspaces = {
-      {
-        name = "personal",
-        path = "~/vaults/personal",
-      },
-      -- {
-      --   name = "work",
-      --   path = "~/vaults/work",
-      -- },
-    },
+    workspaces = present_workspaces,
   },
 }
